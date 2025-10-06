c910847d7fde66fd5dbecb78a172d06a4a26326b
SurgeryMastersAcademy <drbishoy@surgerymastersacademy.com>
2025-10-06 04:11:49 +0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,6 +1,105 @@
-// js/state.js (FINAL CORRECTED VERSION FOR MULTI-PLANNER)
+// js/state.js (Version 1.1 - Upgraded Matching Exam State)
 
 export const API_URL = 'https://script.google.com/macros/s/AKfycbzx8gRgbYZw8Rrg348q2dlsRd7yQ9IXUNUPBDUf-Q5Wb9LntLuKY-ozmnbZOOuQsDU_3w/exec'; // <-- Don't forget to put your NEW URL here!
+
+export const SIMULATION_Q_COUNT = 100;
+export const SIMULATION_TOTAL_TIME_MINUTES = 120;
+export const DEFAULT_TIME_PER_QUESTION = 45;
+export const AVATAR_BASE_URL = "https://api.dicebear.com/7.x/initials/svg?seed=";
+export const AVATAR_OPTIONS = [ "User", "Avatar", "Profile", "Person", "Guest", "Student", "Doctor", "Nurse", "Medical", "Health", "Science", "Knowledge", "Book", "Study", "Exam", "Quiz", "Success", "Champion", "Winner", "Learner" ];
+
+export const appState = {
+    // Content Data
+    allQuestions: [],
+    allFreeTestQuestions: [],
+    allOsceCases: [],
+    allOsceQuestions: [],
+    groupedLectures: {},
+    mcqBooks: [],
+    allAnnouncements: [],
+    allRoles: [],
+    allChaptersNames: [],
+    allTheoryQuestions: [],
+    
+    // User Data
+    currentUser: null,
+    userCardData: null,
+    viewedLectures: new Set(),
+    bookmarkedQuestions: new Set(),
+    answeredQuestions: new Set(),
+    fullActivityLog: [],
+    userQuizNotes: [],
+    userLectureNotes: [],
+    userMessages: [],
+    userTheoryLogs: [],
+    studyPlans: [], 
+    activeStudyPlan: null, 
+
+    // Navigation & UI State
+    navigationHistory: [],
+    activityChartInstance: null,
+    currentNote: { type: null, itemId: null, itemTitle: null },
+    modalConfirmAction: null,
+    qbankSearchResults: [],
+    messengerPollInterval: null,
+
+    // Current Quiz State
+    currentQuiz: {
+        questions: [],
+        originalQuestions: [],
+        userAnswers: [],
+        originalUserAnswers: [],
+        currentQuestionIndex: 0,
+        score: 0,
+        timerInterval: null,
+        simulationTimerInterval: null,
+        flaggedIndices: new Set(),
+        isReviewMode: false,
+        isSimulationMode: false,
+        isPracticingMistakes: false,
+        timePerQuestion: 45,
+    },
+
+    // Current OSCE State
+    currentOsce: {
+        cases: [],
+        caseIndex: 0,
+        questionIndex: 0,
+        timerInterval: null,
+        userAnswers: {},
+        score: 0,
+        totalQuestions: 0,
+    },
+
+    // Current Learning Mode State
+    currentLearning: {
+        questions: [],
+        currentIndex: 0,
+        title: ''
+    },
+
+    // Current Theory Session State
+    currentTheorySession: {
+        questions: [],
+        currentIndex: 0,
+        isExamMode: false,
+        title: '',
+        timerInterval: null
+    },
+
+    // --- NEW: Upgraded Matching Exam State ---
+    currentMatchingExam: {
+        sets: [], // An array of sets, where each set has { premises, answers, questions }
+        currentSetIndex: 0,
+        totalSets: 0,
+        timerInterval: null,
+        userMatches: [], // Array of match objects, one for each set
+        flaggedSets: new Set(),
+        title: '',
+        isReviewMode: false,
+        finalScore: 0
+    },
+};
 
 export const SIMULATION_Q_COUNT = 100;
 export const SIMULATION_TOTAL_TIME_MINUTES = 120;
@@ -101,3 +200,4 @@ export const appState = {
         chapter: '',
     },
 };
+
