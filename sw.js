94b0a6e18bd32a2ad40840313b8b2eee54f970ed
SurgeryMastersAcademy <drbishoy@surgerymastersacademy.com>
2025-11-20 21:46:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js (FINAL FIXED VERSION v4)
 
 // Increased version to force update and fix Tailwind CORS error
-const CACHE_NAME = 'plasticology-app-v15.3'; 
+const CACHE_NAME = 'plasticology-app-v15.4'; 
 
 const ASSETS_TO_CACHE = [
   './',
